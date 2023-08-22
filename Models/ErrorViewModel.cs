96a0a5f9aa4ed928416602a6fd735ad5b6d486af
David Stoker <david.stoker@mast-ict.com>
2023-08-22 08:50:38 +0100

    Add project files 2.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace payment_gateways.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
